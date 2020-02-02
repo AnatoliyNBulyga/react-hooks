fbfb720a33414ac94ef8ab91932e777fc77c616f
AnatoliyNBulyga <anatoliynbulyga@gmail.com>
2020-02-02 19:10:48 +0200

    103 end hooks

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React, {Component, useState, useEffect} from "react";
+import React, {Component, useState, useEffect, useCallback, useMemo} from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(1);
   const [visible, setVisible] = useState(true);
 
   if (visible) {
@@ -14,13 +14,64 @@ const App = () => {
                   onClick={() => setVisible(false)}>hide</button>
               {/*<ClassCounter value={value} />*/}
               {/*<HookCounter value={value} />*/}
-              <Notification/>
+              {/*<Notification/>*/}
+              <PlanetInfo id={value} />
           </div>
       );
   } else {
       return <button
       onClick={() => setVisible(true)}>show</button>
   }
+}
+const getPlanet =  (id) => {
+    return fetch(`https://swapi.co/api/planets/${id}/`)
+        .then(res => res.json())
+        .then(data => data);
+}
+const useRequest = (request) => {
+    const initialState = useMemo(() => ({
+        data: null,
+        loading: true,
+        error: null
+    }), []);
+    const [ dataState, setDataState] = useState(initialState);
+    useEffect(() => {
+        setDataState(initialState);
+        let cancelled = false;
+        request()
+            .then(data => !cancelled && setDataState({
+                data,
+                loading: false,
+                error: null
+            }))
+            .catch(error => !cancelled && setDataState({
+                data: null,
+                loading: false,
+                error
+            }));
+
+        return () => cancelled = true;
+    }, [ request, initialState ]);
+    return dataState;
+}
+
+const usePlanetInfo = (id) => {
+    const request = useCallback(() => getPlanet(id), [id]);
+    return useRequest(request);
+}
+const PlanetInfo = ({id}) => {
+    const { data, loading, error } = usePlanetInfo(id);
+
+    if (error) {
+        return <div>Somthing is wrong</div>
+    }
+    if (loading) {
+        return <div>Loading...</div>
+    }
+    
+    return (
+        <div>{id} - {data.name}</div>
+    )
 }
 const HookCounter = ({value}) => {
     useEffect(() => {
